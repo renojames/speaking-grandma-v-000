5b0e586284ee9b69250c856e73d1be83bf13fd51
Reno Sorensen <renojames19@gmail.com>
2018-02-08 03:11:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(say)
     "HUH?! SPEAK UP, SONNY!"
   elsif say == say.upcase
     "NO, NOT SINCE 1938!"
-  elsif say = "I LOVE YOU GRANDMA!"
+  else
     "I LOVE YOU TOO PUMPKIN!"
   end
 end
